c18e1db7ca246590c19de4b620a890b75ea967c0
apryamostanov <anton.pryamostanov@gmail.com>
2017-02-23 19:41:26 +0400

    Z

--- src/com/a9ae0b01f0ffc/tpn/main/T_tpn_context.groovy
+++ src/com/a9ae0b01f0ffc/tpn/main/T_tpn_context.groovy
@@ -1,7 +1,7 @@
 package com.a9ae0b01f0ffc.tpn.main
 
+import com.a9ae0b01f0ffc.black_box.annotations.I_black_box
 import com.a9ae0b01f0ffc.black_box.main.T_s
-import com.a9ae0b01f0ffc.commons.ioc.T_class_loader
 import com.a9ae0b01f0ffc.commons.main.T_common_context
 import groovy.sql.Sql
 
@@ -9,7 +9,6 @@ class T_tpn_context {
 
     protected static ThreadLocal<T_tpn_context> p_context_thread_local = new ThreadLocal<T_tpn_context>()
     private Sql p_sql = T_tpn_const.GC_NULL_OBJ_REF as Sql
-    private T_class_loader p_ioc = T_tpn_const.GC_NULL_OBJ_REF as T_class_loader
     private T_tpn_commons p_commons = T_tpn_const.GC_NULL_OBJ_REF as T_tpn_commons
 
     private static void check_init() {
@@ -21,7 +20,6 @@ class T_tpn_context {
     void init_custom(String i_commons_conf_file_name) {
             check_init()
             p_context_thread_local.get().p_commons = new T_tpn_commons(i_commons_conf_file_name)
-            p_context_thread_local.get().p_ioc = new T_class_loader(T_tpn_s.c().GC_TPN_CLASSES_CONF)
             T_common_context.get_context().init_custom(i_commons_conf_file_name)
             T_s.x().init_custom(T_tpn_s.c().GC_BLACK_BOX_CONFIG)
     }
@@ -31,11 +29,6 @@ class T_tpn_context {
             get_context().p_sql = Sql.newInstance(T_tpn_s.c().GC_MYSQL_CONNECTION_STRING, T_tpn_s.c().GC_MYSQL_USERNAME, T_tpn_s.c().GC_MYSQL_PASSWORD, T_tpn_s.c().GC_MYSQL_DRIVER)
             get_context().p_sql.getConnection().setAutoCommit(T_tpn_const.GC_FALSE)
         }
-    }
-
-    T_class_loader get_ioc() {
-        check_init()
-        return p_context_thread_local.get().p_ioc
     }
 
     T_tpn_commons get_commons() {
@@ -48,6 +41,7 @@ class T_tpn_context {
         return p_context_thread_local.get()
     }
 
+    @I_black_box
     void sql_update(String i_sql_string, String... i_bind_variables = T_tpn_const.GC_SKIPPED_ARGS) {
         check_init_sql()
         T_s.l().log_sql("update", i_sql_string, i_bind_variables)
