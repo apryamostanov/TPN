b44ea03e67fe71eed483e0651f0c73070d2f54b6
apryamostanov <anton.pryamostanov@gmail.com>
2017-03-31 19:06:17 +0400

    Good

--- src/com/a9ae0b01f0ffc/tpn/main/T_tpn_base_6_util.groovy
+++ src/com/a9ae0b01f0ffc/tpn/main/T_tpn_base_6_util.groovy
@@ -7,9 +7,10 @@ import com.a9ae0b01f0ffc.black_box.main.T_logging_base_5_context
 import groovy.sql.Sql
 
 @I_fix_variable_scopes
-class T_tpn_base_6_util extends T_tpn_base_5_context{
+class T_tpn_base_6_util extends T_tpn_base_5_context {
 
-    @I_black_box("error")//orig=
+    @I_black_box("error")
+//orig=
     static void sql_update(String i_sql_string, Object... i_bind_variables = GC_SKIPPED_ARGS) {
         l().log_send_sql("update", i_sql_string, i_bind_variables)
         if (method_arguments_present(i_bind_variables)) {
@@ -20,19 +21,24 @@ class T_tpn_base_6_util extends T_tpn_base_5_context{
         l().log_receive_sql("updated", get_sql().getUpdateCount())
     }
 
-    @I_black_box("error")//orig=
+    @I_black_box("error")
+//orig=
     static void commit() {
         l().log_send_sql("commit")
         get_sql().commit()
         l().log_receive_sql("commit_finished")
     }
 
-    @I_black_box("error")//orig=
+    @I_black_box("error")
+//orig=
     static Sql get_sql() {
+        //we rely on MySQL property "autoreconnect" in config files
+        get_context().p_sql = Sql.newInstance(c().GC_MYSQL_CONNECTION_STRING, c().GC_MYSQL_USERNAME, c().GC_MYSQL_PASSWORD, c().GC_MYSQL_DRIVER)
         return get_context().p_sql
     }
 
-    @I_black_box("error")//orig=("error")
+    @I_black_box("error")
+//orig=("error")
     static T_logger l() {
         return T_logging_base_5_context.l()
     }
